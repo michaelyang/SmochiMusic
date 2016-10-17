0729d0f26b2bb1f677dd2acf777581b5991bd289
yangmike92 <myang@Michael-Yang.local>
2016-10-17 18:01:58 -0400

    adding add
    adding add

---
+++ add.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+import os
+import re
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.compat import xmlrpc_client
+from wordpress_xmlrpc.methods import media, posts
+
+artistPath = '/Users/myang/Sync/Artists'
+wp_url = 'http://www.smochimusic.com/xmlrpc.php'
+wp_username = 'yangmike'
+wp_password = 'Mxbi9gf8n'
+
+wp = Client(wp_url,wp_username,wp_password)
+mediaLibrary = wp.call(media.GetMediaLibrary({}))
+
+#mediaLibrary = client.methods.media.GetMediaLibrary()
+#wp.call(media.GetMediaLibrary())
+#Needs to be a check/document that records which artist+album has already been added
+#Perhaps a csv or something
+def check(artist,album):
+	return true
+
+#Returns a list of paths to files with extensions as defined in ext
+#Also disregards any temp files created by Mac OS which should technically be hidden
+def getArtistList(rootdir):
+	return [name for name in os.listdir(rootdir) if os.path.isdir(os.path.join(rootdir,name))]
+
+def getAlbumList(artist):
+	rootdir = os.path.join(artistPath,artist,'Albums')
+	return [name for name in os.listdir(rootdir) if os.path.isdir(os.path.join(rootdir,name))]
+
+#albumType should be 'single' or 'full'
+#postType should be 'single' or 'bundle'
+def getSlug(albumType, postType, artist, album):
+	if postType == 'single':
+		prefix = albumType
+	else:
+		prefix = albumType + '-album'
+	return (prefix + '-' + re.sub('[!|(|)|.|,]','',artist) + '-' + re.sub('[!|(|)|.|,]','',album)).replace(' ','-').lower()
+
+#Check if media is already uploaded on WP
+#YES: return url to image
+#NO: Upload image, return url to image
+def getArtwork(artist, album):
+	filePath = os.path.join(artistPath,artist,'Albums','cover.jpg')
+	name = artist + ' - ' + album
+	'''
+	for media in mediaLibrary:
+		if (media.name() == name):
+			return media.url()
+	'''
+	data = {
+		'name': artist + ' - ' + album,
+		'type': 'image/jpeg',  # mimetype
+	}
+	#with open(filePath, 'rb') as img:
+		#pass
+		#data['bits'] = xmlrpc_client.Binary(img.read())
+	#response = client.call(media.UploadFile(data))
+	return data['name']
+	#return response['url']
+
+#Combines two text files to make an html with formatting
+def getDescription(artist, album):
+	lyricsPath = os.path.join(artistPath,artist,'Albums',album,'lyrics.txt')
+	translationPath = os.path.join(artistPath,artist,'Albums',album,'translation.txt')
+	with open(lyricsPath, 'rb') as lyrics:
+		pass
+	with open(translationPath, 'rb') as translation:
+		pass
+	'''
+	&nbsp;	
+	<div class="left" style="text-align: center; font-family: 'Nanum Gothic'; font-size: 13px;">
+	<center><b>
+	#korean artist - korean title
+	</b></center>
+	#lyrics
+	</div>
+	<div class="right" style="text-align: center; font-size: 14px;">
+	<center><b>
+	#english artist - english title
+	</b></center>
+	#translation
+	</div>
+	&nbsp;
+	'''
+	return
+
+def utf_translate(in_string):
+    ''' Gets rid of the BAD GUYS. AKA non-ASCII utf-8 characters. Or at least it tries to.
+    Danaanananananananaa BATMAN.
+    '''
+    in_string = in_string.encode('utf-8').decode('utf-8')
+    #print in_string
+    common_bad_guys={u'—':u'-',#MDASH
+    u'"':u'"',#stupid double left quote
+    u'"':u'"',#stupid double right quote
+    u''':u"'",#stupid left quote
+    u''':u"'",#stupid right quote
+    u'–':u'-',#NDASH?
+    u'…':u'...',#ellipsis
+    u'‐':u'-',#NOT SURE
+    u'‒':u'-',#also NOT SURE
+    u'©':u'&copy;',#copyright symbol,
+    u'®':u'&reg;',#registered symbol
+    u'�':u'?',#weird ?
+    u'™':u'&#8482;',
+    u'"':u'\\\"',
+    u"'":u"\\\'",
+    u'·':u'-',
+    u'\u2019':u"'",
+    u'\u2018':u"'",
+    u"'":u"'",
+    u"'":u"'",
+    u"\u2026":u'...',
+    u'\u201d':u'"'
+    }
+    for key in common_bad_guys:
+        in_string = in_string.replace(key,common_bad_guys[key])
+    return in_string
+
+#generateCSV using all of the info above
+def getCSV():
+	return
+######################################################
+
+artistList = getArtistList(artistPath)
+for artist in artistList:
+	print (artist)
+	albumList = getAlbumList(artist)
+	for album in albumList:
+		print (getSlug('single','bundle',artist,album))
+		print (getArtwork(artist,album))
+		print (getDescription(artist,album))
+
+print ('This is it for the artists')
+
+for media in mediaLibrary:
+	print (media.id)
+	print (len(media.title))
+	print (utf_translate(media.title))
+
+
+'''		1. create a csv that only contains default fields supported by EDD
+		2. create another script that goes through all these shit to set piklist fields?
+		mysql
+		wp9r_postmeta <<piklistshit <<if picklist has their own shit to help us edit this less hackily
+		3. post id = object_id in wp9r_term_relationship, need to match artist id with song id (each artist is a term, use wordpress funcs to relate post to a term (aritst))
+		4. Scarper for melon << lmao
+		5. use jenny to translate
+		6. ????
+		7. profit
+		'''
+
